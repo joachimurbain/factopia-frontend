124f95a19fba1a843e456ee549ae7bc7c0185c17
Joachim Urbain <joachimurbain.git@proton.me>
2025-04-06 10:16:19 +0200

    cleanup Tiktok

--- src/app/pages/tiktok-page/tiktok.page.ts
+++ src/app/pages/tiktok-page/tiktok.page.ts
@@ -19,86 +19,26 @@ import { MenuBarComponent } from '../../components/menu-bar/menu-bar.components'
         />
       </div>
       } @else{
-       
-    <div class="h-screen w-full max-h-screen flex flex-col">
-      <app-menu-bar />
-      <div class="p-4 flex-1">
-        <div class="h-full w-full rounded-xl overflow-hidden">
-          <app-tiktok-video
-            [question]="questions()[currentVideoIndex]"
-            (answerSubmitted)="onAnswerSubmitted()" />
+
+      <div class="h-screen w-full max-h-screen flex flex-col">
+        <app-menu-bar />
+        <div class="p-4 flex-1">
+          <div class="h-full w-full rounded-xl overflow-hidden">
+            <app-tiktok-video
+              [question]="questions()[currentVideoIndex]"
+              (answerSubmitted)="onAnswerSubmitted()"
+            />
+          </div>
         </div>
       </div>
-    </div>
       }
     </div>
   `,
 })
-export class TiktokPageComponent implements OnInit {
+export class TiktokPageComponent {
   public currentVideoIndex = 0;
   public questionService = inject(QuestionService);
-
-  ngOnInit(): void {
-    // this.questionService.getAllVideos().subscribe((videos) => {
-    //   this.videos = videos;
-    // });
-  }
-
-  // onAnswerSubmitted() {
-  //   this.nextVideo();
-  // }
   public questions = computed(() => this.questionService.items());
-
-  // public questions = computed(() => {
-  //   return [
-  //       {
-  //           id: 1,
-  //           content: "Regarde et decide si c'est vrai",
-  //           gameType: {
-  //               id: 1,
-  //               title: "TikTok",
-  //               description: "Regarde la video ensuite, swipe a droite si c'est vrai !"
-  //           },
-  //           resource: {
-  //               id: 1,
-  //               name: "A_1.mp4",
-  //               path: "/videos/A_1.mp4",
-  //               resourceType: ResourceType.video,
-  //               isCorrect: false,
-  //               questionId: 1
-  //           },
-  //           answer: {
-  //               id: 1,
-  //               content: "This video is fake",
-  //               isCorrect: false,
-  //               questionId: 1
-  //           }
-  //       },
-  //       {
-  //           id: 2,
-  //           content: "Est-ce que cette vidéo est authentique?",
-  //           gameType: {
-  //               id: 1,
-  //               title: "TikTok",
-  //               description: "Regarde la video ensuite, swipe a droite si c'est vrai !"
-  //           },
-  //           resource: {
-  //               id: 2,
-  //               name: "A_2.mp4",
-  //               path: "/videos/A_2.mp4",
-  //               resourceType: ResourceType.video,
-  //               isCorrect: true,
-  //               questionId: 2
-  //           },
-  //           answer: {
-  //               id: 2,
-  //               content: "This video is real",
-  //               isCorrect: true,
-  //               questionId: 2
-  //           }
-  //       }
-  //   ] as Question[];
-  // });
 
   constructor() {
     this.questionService.loadByGameType(GameTypes.TikTok);
